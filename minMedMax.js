027752b895268f64d5518418fee1cc54170449ed
phongsathon-chansongkrao <kitokidandkwa@gmail.com>
2022-09-12 12:56:00 +0700

    version1

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,15 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  if(n1>n2&&n2>n3&&n1>n3)
+    return n3,n2,n1
+  else if (n1>n2&&n3>n2&&n1>n3)
+    return n2,n3,n1
+  else if (n1<n2&&n3<n2&&n3>n1)
+    return n1,n3,n2
+  else if (n1<n2&&n3<n2&&n1>n3)
+    return n3,n1,n2
+  else if (n1>n2??n2>n3)
+
 }
 module.exports = minMedMax
